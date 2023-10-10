234246e7e52d8a4e2e23607b5e86884279ac9cc0
‘aja <aja.onyebuchi@yahoo.com>
2023-10-10 16:02:01 +0100

    create application endpoint

---
+++ springBoot-docker-demo/src/test/java/com/bujubanton/springBoot/docker/demo/SpringBootDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bujubanton.springBoot.docker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
